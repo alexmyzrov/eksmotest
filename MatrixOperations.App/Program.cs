b43cd4678d9d48e89c66ecf60240c350b87a94c2
Alex My <alexmy@outlook.com>
2019-06-18 22:14:04 +0300

    App stub

---
+++ MatrixOperations.App/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MatrixOperations.App
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Please enter a path to tasks folder.");
+                return;
+            }
+            
+            Console.WriteLine(args[0]);
+        }
+    }
+}
